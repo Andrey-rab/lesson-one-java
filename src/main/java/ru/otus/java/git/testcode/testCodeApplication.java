00a020a0935759e323be5ad90a1288bcc146f127
Andrey <andreyserko9@gmail.com>
2025-01-12 22:21:52 +0300

    init

---
+++ src/main/java/ru/otus/java/git/testcode/testCodeApplication.java
@@ -0,0 +1,22 @@
+package ru.otus.java.git.testcode;
+
+public class testCodeApplication {
+    // comment
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        if (a > 10) {
+            System.out.println("Значение переменной a строго больше 10");
+        }
+        if (a != 10) {
+            System.out.println("Значение переменной a не равно 10");
+        }
+        if (sum >= 0) {
+            System.out.println("Сумма a и b больше либо равна 0");
+        }
+        if (a == b) {
+            System.out.println("Значение переменной a равно значению переменной b");
+        }
+    }
+}
